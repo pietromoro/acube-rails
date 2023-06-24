ad060484a93e87add97048552de70e03e61f82d6
Pietro Moro <pietro@pietromoro.dev>
2023-06-24 17:47:02 +0200

    Scaffold out concerns api

---
+++ lib/acube/concerns/consumer.rb
@@ -0,0 +1,7 @@
+module ACube
+  module Consumer
+    module Model
+      extend ActiveSupport::Concern
+    end
+  end
+end
---
+++ lib/acube/concerns/supplier.rb
@@ -0,0 +1,7 @@
+module ACube
+  module Supplier
+    module Model
+      extend ActiveSupport::Concern
+    end
+  end
+end
