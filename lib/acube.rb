f35d20d7e1b895e6a7a6ea4115f2cc9434b0b5d2
Pietro Moro <pietro@pietromoro.dev>
2023-06-24 17:20:33 +0200

    Base configurations

--- lib/acube.rb
+++ lib/acube.rb
@@ -5,4 +5,19 @@ require "acube/version"
 
 module ACube
   extend ActiveSupport::Autoload
+
+  mattr_accessor :invoice_endpoint
+  mattr_accessor :common_endpoint
+
+  mattr_accessor :invoice_base_class, default: "ApplicationRecord"
+  
+  mattr_accessor :webhook_base_class, default: "ApplicationController"
+  mattr_accessor :webhook_endpoint, default: "/acube-api/webhook"
+  mattr_accessor :webhook_secret_key
+  mattr_accessor :webhook_secret
+
+  mattr_accessor :webhook_signature_key
+  mattr_accessor :webhook_signature_gpg
+  
+  mattr_accessor :auth_token_cache_key, default: "__acube__auth__token"
 end
