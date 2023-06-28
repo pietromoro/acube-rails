e55a8aeea3218594638a44378a724d1f4ddbff4b
Pietro Moro <pietro@pietromoro.dev>
2023-06-28 02:03:50 +0200

    Reflect belongs to in the attribute concern

--- lib/acube/attribute.rb
+++ lib/acube/attribute.rb
@@ -3,11 +3,18 @@ module ACube
     extend ActiveSupport::Concern
 
     class_methods do
-      def has_invoice(name)
+      def has_invoice(name, strict_loading: strict_loading_by_default)
         class_eval <<-CODE, __FILE__, __LINE__ + 1
+          def #{name}
+            invoice_#{name} || build_invoice_#{name}
+          end
+
+          def #{name}?
+            invoice_#{name}.present?
+          end
         CODE
 
-        has_one :"invoice_#{name}", -> { where(name: name) }
+        has_one :"invoice_#{name}", -> { where(name: name) }, class_name: 'ACube::InvoiceRecord', as: :record, inverse_of: :record, autosave: true, dependent: :destroy, strict_loading: strict_loading
       end
     end
   end
