db5db1da11cd821c981023860d59609781605185
Pietro Moro <pietro@pietromoro.dev>
2023-06-28 13:37:08 +0200

    Customer xml

--- lib/acube/schema/header/customer.rb
+++ lib/acube/schema/header/customer.rb
@@ -7,8 +7,33 @@ module ACube
         attr_accessor :first_name, :last_name, :denomination, :title, :eori_code
         attr_accessor :address, :civic_number, :zip, :city, :province, :nation
 
+        def from(supplier)
+          supplier.supplier_data.each do |key, value|
+            value = value.is_a?(Symbol) ? supplier.send(value).to_s : value.to_s
+            send("#{key}=", value)
+          end
+        end
+
         def to_xml
-          
+          fragment = Nokogiri::XML::DocumentFragment.new(Nokogiri::XML::Document.new)
+          Nokogiri::XML::Builder.with(fragment) do |xml|
+            xml.CessionarioCommittente {
+              xml.DatiAnagrafici {
+                xml.CodiceFiscale fiscal_code
+                xml.Anagrafica {
+                  xml.Nome first_name
+                  xml.Cognome last_name
+                }
+              }
+              xml.Sede {
+                xml.Indirizzo address
+                xml.NumeroCivico civic_number
+                xml.CAP zip
+                xml.Provincia province
+                xml.Nazione nation
+              }
+            }
+          end
         end
       end
     end
