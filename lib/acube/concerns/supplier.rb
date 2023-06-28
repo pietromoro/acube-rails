10a5b84748782171ec560f3562e026dd57a90fcd
Pietro Moro <pietro@pietromoro.dev>
2023-06-28 12:13:51 +0200

    Supplier concern

--- lib/acube/concerns/supplier.rb
+++ lib/acube/concerns/supplier.rb
@@ -2,6 +2,43 @@ module ACube
   module Supplier
     module Model
       extend ActiveSupport::Concern
+      cattr_accessor(:supplier_data) { {} }
+
+      
+      class SupplierBuilder
+        @@attributes = ACube::Schema::Header::Supplier.instance_methods.select {|m| m.ends_with?("=") && m.starts_with?(/\w/) }
+        @supplier_data = {}
+
+        def initialize
+          @supplier_data = {}
+        end
+
+        def finalize
+          @supplier_data
+        end
+        
+        def method_missing(method, value)
+          if (@@attributes.include?(method))
+            @supplier_data[method[0..-2]] = value
+            puts @supplier_data
+          else
+            super 
+          end
+        end
+      end
+
+      class_methods do
+      protected
+        def as_supplier(&block)
+          config = SupplierBuilder.new
+          yield(config)
+          supplier_data = config.finalize
+        end
+      end
+
+      def to_supplier
+        ACube::Schema::Header::Supplier.new.from(self)
+      end
     end
   end
 end
