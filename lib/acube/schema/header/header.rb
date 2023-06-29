0e0d455843b065ba071f9f953672a7f13913ec51
Pietro Moro <pietro@pietromoro.dev>
2023-06-29 17:18:20 +0200

    Fix header structure

--- lib/acube/schema/header/header.rb
+++ lib/acube/schema/header/header.rb
@@ -19,12 +19,14 @@ module ACube
                   xml.IdPaese ACube.transmission_nation_id
                   xml.IdCodice ACube.transmission_id_code
                 }
+
                 xml.ProgressivoInvio progressive
                 xml.FormatoTrasmissione transmission_format
                 xml.CodiceDestinatario "0000000"
-                xml << supplier.to_xml
-                xml << customer.to_xml
               }
+              
+              xml << supplier.to_xml
+              xml << customer.to_xml
             }
           end.to_xml(save_with: 2)
         end
