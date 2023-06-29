a42a29a55a40adfac2c9fa3cfc913dec431f6569
Pietro Moro <pietro@pietromoro.dev>
2023-06-29 04:53:50 +0200

    SignatureChecker refactor [WIP]

--- lib/acube/signature_checker.rb
+++ lib/acube/signature_checker.rb
@@ -1,5 +1,7 @@
 module ACube
   class SignatureChecker
+    class InvalidSignatureError < StandardError; end
+
     HASH_ALGORIGHTM = "sha256"
     DEFAULT_KEY = "acube"
     DEFAULT_GPG = <<-GPG
@@ -8,14 +10,22 @@ module ACube
       -----END PUBLIC KEY-----
     GPG
 
-    def self.verify_signature(url, params)
-      new(url, params).verify_signature
+    def self.verify_signature(request, payload)
+      new(request.headers, payload).verify_signature
     end
 
-    attr_reader :url, :params
-    def initialize(url, params)
-      @url = url
-      @params = params
+    attr_reader :headers, :payload
+    attr_accessor :signature, :input, :digest
+    def initialize(headers, payload)
+      @headers = headers
+      if (!ACube.webhook_secret_key.nil? && !ACube.webhook_secret.nil?)
+        raise InvalidSignatureError unless headers.has_key?("Authorization") && headers["Authorization"] == "#{ACube.webhook_secret_key} #{ACube.webhook_secret}"
+      end
+
+      raise InvalidSignatureError unless headers.has_key?("signature") && headers.has_key?("signature-input") && headers.has_key?("signature-digest")
+      signature, input, digest = headers["signature"], headers["signature-input"], headers["signature-digest"]
+
+      @payload = payload
     end
 
     def verify_signature
